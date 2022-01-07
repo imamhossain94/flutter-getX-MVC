12f5bf78c86c35390ea30f72482e701a602e923b
imamhossain94 <imamagun94@gmail.com>
2022-01-07 08:37:08 +0600

    feat: TodoModel Created

---
+++ lib/models/todo_model.dart
@@ -0,0 +1,10 @@
+class TodoModel {
+  String? title;
+  String? subTitle;
+
+  TodoModel({title = "Hello Programmer!!", subTitle = "Welcome To Flutter GetX MVC Arc."}){
+    title = title;
+    subTitle = subTitle;
+  }
+  
+}
