43801bcaa1807dbfc2b6610c0bacc13ac90bac45
imamhossain94 <imamagun94@gmail.com>
2022-01-07 08:13:13 +0600

    feat: todopage created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_mvc/views/todo_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      //home: ShoppingPage(),
+      home: TodoPage(),
     );
   }
 }
